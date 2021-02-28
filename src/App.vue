a18d34b947ac2bdb11dda9a8f70ef4a8b1a0e5e5
fedorovcev <fedorovcev@g-soft.ru>
2021-02-28 22:19:59 +0300

    added pagination

--- src/App.vue
+++ src/App.vue
@@ -90,10 +90,31 @@
       </section>
 
       <template v-if="tickers.length">
+        <div>
+          <hr class="w-full border-t border-gray-600 my-4" />
+          <button
+            @click="page--"
+            v-if="page > 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            @click="page++"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперед
+          </button>
+          <div>
+            Фильтр:
+            <input v-model.trim="filter" />
+          </div>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -187,10 +208,26 @@ export default {
       ticker: "",
       tickers: [],
       sel: null,
-      graph: []
+      graph: [],
+      filter: "",
+      page: 1,
+      recordsPerPage: 6,
+      hasNextPage: false
     };
   },
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * this.recordsPerPage;
+      const end = this.page * this.recordsPerPage;
+
+      const filteredTickers = this.tickers.filter(tkr =>
+        tkr.name.toLowerCase().includes(this.filter.toLowerCase())
+      );
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
     subscribeToUpdates(tickerName) {
       // setInterval(async () => {
       //   const f = await fetch(
@@ -215,7 +252,7 @@ export default {
     },
     addTicker() {
       const currentTicker = {
-        name: this.ticker,
+        name: this.ticker.toUpperCase(),
         price: "-"
       };
 
@@ -228,6 +265,7 @@ export default {
       localStorage.setItem("crypton", JSON.stringify(this.tickers));
 
       this.subscribeToUpdates(currentTicker.name);
+      this.filter = "";
     },
 
     checkExistingTickers() {
@@ -275,7 +313,37 @@ export default {
       this.graph = [];
     }
   },
+  watch: {
+    filter() {
+      this.page = 1;
+
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    }
+  },
   async created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.fitler = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const allCoins = await fetch(
       "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
     )
