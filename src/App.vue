2968f196e00778df72d2ab39975fc4b01f2888e5
fedorovcev <fedorovcev@g-soft.ru>
2021-02-28 18:07:33 +0300

    add localStorage

--- src/App.vue
+++ src/App.vue
@@ -191,6 +191,24 @@ export default {
     };
   },
   methods: {
+    subscribeToUpdates(tickerName) {
+      // setInterval(async () => {
+      //   const f = await fetch(
+      //     `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=9c60c6d2022f78e67710e55f58b688115870d7cad053780af466b8614e2efb8b`
+      //   );
+      //   const data = await f.json();
+      //   console.log(data.USD);
+
+      //   this.tickers.find(t => t.name === tickerName).price =
+      //     data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+      //   if (this.sel?.name === tickerName) {
+      //     this.graph.push(data.USD);
+      //   }
+      // }, 5000);
+      this.ticker = "";
+      this.suggestCoins = [];
+    },
     addTickerToInput({ target }) {
       this.ticker = target.innerText;
       this.addTicker();
@@ -207,23 +225,9 @@ export default {
 
       this.tickers.push(currentTicker);
 
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=9c60c6d2022f78e67710e55f58b688115870d7cad053780af466b8614e2efb8b`
-        );
-        const data = await f.json();
-        console.log(data.USD);
+      localStorage.setItem("crypton", JSON.stringify(this.tickers));
 
-        this.tickers.find(t => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 5000);
-
-      this.ticker = "";
-      this.suggestCoins = [];
+      this.subscribeToUpdates(currentTicker.name);
     },
 
     checkExistingTickers() {
@@ -286,6 +290,13 @@ export default {
       .catch(e => console.log(e));
 
     this.coinsList = allCoins;
+
+    const tickersData = localStorage.getItem("crypton");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+    }
+
+    this.tickers.forEach(ticker => this.subscribeToUpdates(ticker.name));
   }
 };
 </script>
